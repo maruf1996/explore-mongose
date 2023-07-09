54ca3e1e42d2f5ace5ad523d009779cc93116687
maruf1996 <maruf3813@gmail.com>
2023-07-09 13:24:27 +0600

    'second-commit'

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,7 @@ main();
 async function main() {
   try {
     await mongoose.connect("mongodb://127.0.0.1:27017/practice-mongose");
-    console.log("Database connection successfully");
+    console.log("Database connection is successfully");
 
     app.listen(port, () => {
       console.log(`Server is listening on port ${port}`);
